979337b06a0f0581c04614ce981acda15aa11e0a
alexeyvkuznetsov <57808789+alexeyvkuznetsov@users.noreply.github.com>
2020-02-13 23:27:28 +0300

    1

--- latin_text_preprocessing.R
+++ latin_text_preprocessing.R
@@ -1,4 +1,11 @@
-
+########################################################################
+########################################################################
+##  Latin Texts Preprocessing                                         ##
+##  Author: Alexey Kuznetsov                                          ##
+##  URL: https://github.com/alexeyvkuznetsov/Latin_Text_Preprocessing ##
+##       https://alexeyvkuznetsov.github.io                           ##
+########################################################################
+########################################################################
 library(tm)
 library(udpipe)
 
@@ -11,39 +18,53 @@ recapitulatio<-paste(scan(file ="files/03 recapitulatio.txt",what='character'),c
 historia_w<-paste(scan(file ="files/04 historia_w.txt",what='character'),collapse=" ")
 historia_s<-paste(scan(file ="files/05 historia_s.txt",what='character'),collapse=" ")
 
-prologus <- tolower(prologus)
-historia_g <- tolower(historia_g)
-recapitulatio <- tolower(recapitulatio)
-historia_w <- tolower(historia_w)
-historia_s <- tolower(historia_s)
-
-prologus <- stripWhitespace(prologus)
-historia_g <- stripWhitespace(historia_g)
-recapitulatio <- stripWhitespace(recapitulatio)
-historia_w <- stripWhitespace(historia_w)
-historia_s <- stripWhitespace(historia_s)
-
 prologus<-data.frame(texts=prologus)
 historia_g<-data.frame(texts=historia_g)
 recapitulatio<-data.frame(texts=recapitulatio)
 historia_w<-data.frame(texts=historia_w)
 historia_s<-data.frame(texts=historia_s)
 
+prologus$book<-"1 Prologus"
+historia_g$book<-"2 Historia Gothorum"
+recapitulatio$book<-"3 Recapitulatio"
+historia_w$book<-"4 Historia Wandalorum"
+historia_s$book<-"5 Historia Suevorum"
 
-prologus$book<-"Prologus"
-historia_g$book<-"Historia_Gothorum"
-recapitulatio$book<-"Recapitulatio"
-historia_w$book<-"Historia_Wandalorum"
-historia_s$book<-"Historia_Suevorum"
+historia<-rbind(prologus,historia_g,recapitulatio,historia_w,historia_s)
+
+#historia$texts <- stripWhitespace(historia$texts)
+historia$texts <- tolower(historia$texts)
+historia$texts <- removePunctuation(historia$texts)
+historia$texts <- removeNumbers(historia$texts)
+
+# Stopwords
+
+load("rome_number_1000.Rda")
+
+customStopWords <- c("ann", "annus", "aer", "aes", "aera", "num._rom.", "xnum._rom.", "xxnum._rom.", "xxxnum._rom.", "cdxlnum._rom.")
+
+lat_stop_perseus <- c("ab", "ac", "ad", "adhic", "aliqui", "aliquis", "an", "ante", "apud", "at", "atque", "aut", "autem", "cum", "cur", "de", "deinde", "dum", "ego", "enim", "ergo", "es", "est", "et", "etiam", "etsi", "ex", "fio", "haud", "hic", "iam", "idem", "igitur", "ille", "in", "infra", "inter", "interim", "ipse", "is", "ita", "magis", "modo", "mox", "nam", "ne", "nec", "necque", "neque", "nisi", "non", "nos", "o", "ob", "per", "possum", "post", "pro", "quae", "quam", "quare", "qui", "quia", "quicumque", "quidem", "quilibet", "quis", "quisnam", "quisquam", "quisque", "quisquis", "quo", "quoniam", "sed", "si", "sic", "sive", "sub", "sui", "sum", "super", "suus", "tam", "tamen", "trans", "tu", "tum", "ubi", "uel", "uero", "unus", "ut", "quoque", "xiix")
+
+#save(lat_stop_perseus,file="lat_stop_perseus.Rda")
+
+#load("lat_stop_perseus.Rda")
+
+#MyStopwords <- c(lat_stop_perseus, customStopWords, lat_stopwords_romnum)
+
+MyStopwords <- c(lat_stop_perseus, rome_number_1000, customStopWords)
+
+#historia$texts <- removeWords(historia$texts, c(lat_stop_perseus, rome_number_1000))
+
+historia$texts <- removeWords(historia$texts, MyStopwords)
+
+historia$texts <- stripWhitespace(historia$texts)
 
 
-fivebooks<-rbind(prologus,historia_g,recapitulatio,historia_w,historia_s)
+
+udmodel_latin <- udpipe_load_model(file = "latin-ittb-ud-2.5-191206.udpipe")
 
 
-udmodel_latin <- udpipe_load_model(file = "latin-ittb-ud-2.4-190531.udpipe")
-
-
-x <- udpipe_annotate(udmodel_latin, x = fivebooks$texts, doc_id = fivebooks$book, tagger = "default", parser = "default", trace = TRUE)
+x <- udpipe_annotate(udmodel_latin, x = historia$texts, doc_id = fivebooks$book, tagger = "default", parser = "default", trace = TRUE)
 x <- as.data.frame(x)
 
 
